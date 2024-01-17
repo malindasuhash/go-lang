8498c4b14db35fe28785179772c619bf6ba6463f
malindasuhash <malinda.suhash@gmail.com>
2024-01-17 06:31:16 +0000

    progressing to paint_calculator.go

--- paint_calculator.go
+++ paint_calculator.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 )
 
 var metersPerLiter float64 // Package level variable
 
 func main() {
 	metersPerLiter = 10.0
-	paint := paintNeeded(4.2, 3.0)
+	paint, err := paintNeeded(4.2, 3.0)
+	if err != nil {
+		log.Fatal(err)
+		return // This is not needed because Fatal will break current flow and stop
+	}
 	fmt.Printf("%.2f liters needed", paint)
-	fmt.Println()
-	paint += paintNeeded(5, 8.0)
-	fmt.Printf("%.2f liters needed", paint)
-	fmt.Println()
 }
 
-func paintNeeded(width float64, height float64) float64 {
+func paintNeeded(width float64, height float64) (float64, error) {
+	var err error = nil // Initialising, not that it matters in this case
+	if width < 0 {
+		// This is one way of creating formatted errors (via errors package)
+		err = errors.New(fmt.Sprintf("Width cannot be negative. It is %5.2f", width))
+	}
+
+	if height < 0 {
+		// This is another way of creating errors
+		err = fmt.Errorf("Height cannot be a negative number. It is %5.2f", height)
+	}
+
+	if err != nil {
+		return 0, err
+	}
+
 	area := width * height
-	return area // Returning from a function of type float64
+	return area, err // Returning from a function of type float64 and an error
+
 }
